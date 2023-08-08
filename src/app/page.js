8a931067fa59323350a0d7a6e051d8ba3d0744ea
akkory <patrickbmarques@hotmail.com>
2023-08-07 21:29:32 -0300

    new thisng

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,69 @@
+"use client";
 import SquaredButton from "./Components/SquaredButton";
 import Display from "./Components/Display";
-import { useState } from "react";
+import {useState} from "react";
 
 export default function Home() {
-  const [display, setDisplay] = useState (initialState:"0")
-
+  const [display, setDisplay] = useState("0")
+  
   function pressButton(numero) {
-    console.info 
+    console.info("pressButton", numero)
+    if (numero === "AC") {
+      setDisplay("0")
+    } else if (numero !== "0" && display === "0") {
+      setDisplay(`${display !== "0" ? display : ""}${numero}`)
+    } else {
+      setDisplay(`${display !== "0" ? display : ""}${numero}`)
+    }
+  }
+  function calculate() {
+    const result = eval(display)
+    setDisplay(result)
+  }
+
+  function backspace() {
+    if (display.length === 1) {
+      setDisplay("0")
+    } else {
+      const rErase = String(display).slice(0, display.length - 1)
+      setDisplay(Boolean(rErase) ? rErase : "0")
+    }
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='space-y-1 p-2 w-64 bg-red-600 rounded'>  
-       <Display />
+      <div className="space-y-1 p-2 w-64 bg-red-600 rounded">  
+       <Display text={display}/>
     
 
       <div className="flex flex-row space-x-1">
-      <SquaredButton numero={"AC"}/>
-      <SquaredButton numero={"X"}/>
-      <SquaredButton numero={"%"}/>
-      <SquaredButton numero={"/"}/>
+      <SquaredButton numero={"AC"} onClick={pressButton}/>
+      <SquaredButton numero={"X"} onClick={pressButton}/>
+      <SquaredButton numero={"%"} onClick={pressButton}/>
+      <SquaredButton numero={"/"} onClick={pressButton}/>
       </div>
       <div className="flex flex-row space-x-1">
-      <SquaredButton numero={7}/>
-      <SquaredButton numero={8}/>
-      <SquaredButton numero={9}/>
-      <SquaredButton numero={"X"}/>
+      <SquaredButton numero={7} onClick={pressButton}/>
+      <SquaredButton numero={8} onClick={pressButton}/>
+      <SquaredButton numero={9} onClick={pressButton}/>
+      <SquaredButton numero={"X"} onClick={pressButton}/>
       </div>
       <div className="flex flex-row space-x-1">
-      <SquaredButton numero={4}/>
-      <SquaredButton numero={5}/>
-      <SquaredButton numero={6}/>
-      <SquaredButton numero={"-"}/>
+      <SquaredButton numero={4} onClick={pressButton}/>
+      <SquaredButton numero={5} onClick={pressButton}/>
+      <SquaredButton numero={6} onClick={pressButton}/>
+      <SquaredButton numero={"-"} onClick={pressButton}/>
       </div>
       <div className="flex flex-row space-x-1">
-      <SquaredButton numero={1}/>
-      <SquaredButton numero={2}/>
-      <SquaredButton numero={3}/>
-      <SquaredButton numero={"+"}/>
+      <SquaredButton numero={1} onClick={pressButton}/>
+      <SquaredButton numero={2} onClick={pressButton}/>
+      <SquaredButton numero={3} onClick={pressButton}/>
+      <SquaredButton numero={"+"} onClick={pressButton}/>
       </div>
       <div className="flex flex-row space-x-1">
-      <SquaredButton numero={""}/>
-      <SquaredButton numero={0}/>
-      <SquaredButton numero={","}/>
-      <SquaredButton numero={"="}/>
+      <SquaredButton numero={""} onClick={pressButton}/>
+      <SquaredButton numero={0} onClick={pressButton}/>
+      <SquaredButton numero={","} onClick={pressButton}/>
+      <SquaredButton numero={"="} onClick={pressButton}/>
       </div>  
       </div>
 
